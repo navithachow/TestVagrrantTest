164aaaedd19ece3aa4c310370b228f7da5990663
navithachow <138020899+navithachow@users.noreply.github.com>
2023-11-25 13:54:40 +0530

    Add files via upload

---
+++ testt.py
@@ -0,0 +1,8 @@
+def shoppingcart(product,price,gst,quantity):
+
+    string product=["leather wallet","umbrella","cigarette","honey"]
+    int prices=["1100","900","200","100"]
+    int gst(18,12,28,0)
+    int quantity={1,4,3,2}
+    
+    
